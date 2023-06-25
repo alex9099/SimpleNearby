83a0ef00342761af873978e01527ed5f089e8102
Alexandre Badalo <alexandre.badalo@sapo.pt>
2023-06-25 17:56:12 +0100

    Added more info about BLE

--- ble_get_devices.py
+++ ble_get_devices.py
@@ -2,7 +2,9 @@ import argparse
 import asyncio
 
 from bleak import BleakScanner
+from bleak import BleakClient
 
+rcvdevices = []
 
 async def main(args: argparse.Namespace):
     print("scanning for 5 seconds, please wait...")
@@ -19,6 +21,14 @@ async def main(args: argparse.Namespace):
         if "0000fef3-0000-1000-8000-00805f9b34fb" in value[1].service_data:
             print(key + " is doing nearby share thingies (likely recieving)")
             print(value[1].service_data["0000fef3-0000-1000-8000-00805f9b34fb"])
+            rcvdevices.append(key)
+
+    #this errors for devices not sharing to everyone
+    for mac in rcvdevices:
+        async with BleakClient(mac) as client:
+            model_number = await client.read_gatt_char("00000000-0000-3000-8000-000000000000")
+            print(model_number)
+            print(''.join(format(x, '02x') for x in model_number))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
